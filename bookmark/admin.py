aec4da8afe8e57c32f30c2418247bf2dd01782cf
ivan <xormrdlsrks2@gmail.com>
2023-06-26 11:26:58 +0900

    View 로직 구축

--- bookmark/admin.py
+++ bookmark/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from bookmark.models import Bookmark
 # Register your models here.
+
+# 테코레이터 패턴 x
+# admin.site.regiter(Bookmark, BookmarkAdmin)
+
+# 데코레인터 패턴 사용
+@admin.register(Bookmark)
+class BookmarkAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "url")
